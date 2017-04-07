ebda06b90eff5aa421c83790d96547039d04868b
Leonardo Tabosa <ltabosa@hotmail.com>
2017-04-07 10:39:14 -0400

    Start Project

---
+++ ExpenseReport/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="b4042a9a-f1f2-4b52-b6c5-647430584bc8" description="Fonctionnalité de complément SharePoint" featureId="b4042a9a-f1f2-4b52-b6c5-647430584bc8" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ExpenseReport Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="53e9ef82-64bf-480f-9de8-eddabdd3e1bc" />
+    <projectItemReference itemId="f3bb460e-d27c-4690-954e-96e47dd262bc" />
+    <projectItemReference itemId="260fa67b-b86e-4d60-9d23-e1363debef10" />
+    <projectItemReference itemId="02089c8e-3b6e-41cf-9e59-cbcd483ff79e" />
+  </projectItems>
+</feature>
---
+++ ExpenseReport/Scripts/App.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+
+ExecuteOrDelayUntilScriptLoaded(initializePage, "sp.js");
+
+function initializePage()
+{
+    var context = SP.ClientContext.get_current();
+    var user = context.get_web().get_currentUser();
+
+    // Ce code s'exécute quand le modèle DOM est prêt. Par ailleurs, il crée un objet de contexte nécessaire à l'utilisation du modèle objet SharePoint
+    $(document).ready(function () {
+        getUserName();
+    });
+
+    // Cette fonction prépare, charge, puis exécute une requête SharePoint pour obtenir des informations sur les utilisateurs actuels
+    function getUserName() {
+        context.load(user);
+        context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+    }
+
+    // Cette fonction est exécutée si l'appel ci-dessus est réussi
+    // Elle remplace le contenu de l'élément 'message' par le nom de l'utilisateur
+    function onGetUserNameSuccess() {
+        $('#message').text('Hello ' + user.get_title());
+    }
+
+    // Cette fonction est exécutée en cas d'échec de l'appel ci-dessus
+    function onGetUserNameFail(sender, args) {
+        alert('Failed to get user name. Error:' + args.get_message());
+    }
+}
